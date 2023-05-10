3a2b5e5f23c0fe15d8244a3d4a5f74d14937bb0a
Marcello Farias <marcellorocha632@gmail.com>
2023-05-10 10:56:01 -0300

    Creating the database connection

---
+++ src/database/database.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+const { DB_PASSWORD } = require('../../config/config');
+
+const dataBase = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: DB_PASSWORD,
+    database: 'dbtodo'
+});
+
+dataBase.connect((error) => {
+    if(error) {
+        throw error; 
+    }
+
+    console.log('Database connected !');
+});
+
+module.exports = dataBase;
